ae9a74c50af81dd7cfc1f1da61b9b6cac4b118e2
ma457775 <matyalpo@gmail.com>
2025-04-30 17:23:44 -0500

    XD

--- main.py
+++ main.py
@@ -14,8 +14,22 @@ def index():
 # Habilidades dinámicas
 @app.route('/', methods=['POST'])
 def process_form():
-    button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_pressed = None
+
+    if 'button_python' in request.form:
+        button_pressed = 'python'
+    elif 'button_discord' in request.form:
+        button_pressed = 'discord'
+    elif 'button_html' in request.form:
+        button_pressed = 'html'
+    elif 'button_db' in request.form:
+        button_pressed = 'db'
+    elif 'button_roblox' in request.form:
+        button_pressed = 'roblox'
+
+    return render_template('index.html', button_pressed=button_pressed)
+
+
 
 
 if __name__ == "__main__":
